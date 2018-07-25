174142ac5ba72c17483eef65447274445ae14148
Slawomir Wojda <slwojda@poczta.onet.pl>
2018-07-25 21:46:29 +0200

    wyswiewtlanie opisu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let paragraf = document.getElementById("opis")
+
+let button = document.querySelector("button");
+
+console.log(paragraf);
+console.log(button);
+
+button.onclick = function () {
+
+    paragraf.innerText = "Akademia 108";
+};
